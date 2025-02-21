f80335f4256977b83c6baf96e6379b6476d05e3c
Fredy Nurman Saleh <email@fredyns.net>
2025-02-21 18:43:21 +0700

    ordering sub tasks

---
+++ database/migrations/2025_02_21_110608_indexing_task_parent.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('shipbuilding_tasks', function (Blueprint $table) {
+            $table->index(['parent_task_id', 'sort_order'],'child_order');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('shipbuilding_tasks', function (Blueprint $table) {
+            $table->dropIndex('child_order');
+        });
+    }
+};
