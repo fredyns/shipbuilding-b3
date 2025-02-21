7e8fcedeba09b61b633ee0bf8a264c401a1020a7
Fredy Nurman Saleh <email@fredyns.net>
2025-02-21 18:01:05 +0700

    task seeder

--- database/seeders/ShipbuildingTaskSeeder.php
+++ database/seeders/ShipbuildingTaskSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
+use App\Models\Shipbuilding;
 use App\Models\ShipbuildingTask;
+use Illuminate\Database\Seeder;
 
 class ShipbuildingTaskSeeder extends Seeder
 {
@@ -12,8 +13,34 @@ class ShipbuildingTaskSeeder extends Seeder
      */
     public function run(): void
     {
-        ShipbuildingTask::factory()
-            ->count(5)
-            ->create();
+        $tugData = include __DIR__ . '/data/ShipbuildingTaskSeeder.php';
+
+        if (!is_array($tugData)) {
+            $this->command->warn('ShipbuildingTaskSeeder empty.');
+            return;
+        }
+
+        $lastID = (int)ShipbuildingTask::max('id');
+        $shipbuildings = Shipbuilding::all();
+        foreach ($shipbuildings as $shipbuilding) {
+            $taskSeeds = $this->adjustID($tugData, $shipbuilding, $lastID);
+            ShipbuildingTask::insert($taskSeeds);
+
+            $lastID += count($taskSeeds);
+        }
+    }
+
+    protected function adjustID($taskSeeds, Shipbuilding $shipbuilding, $lastID)
+    {
+        foreach ($taskSeeds as &$taskSeed) {
+            $taskSeed['id'] += $lastID;
+            $taskSeed['shipbuilding_id'] = $shipbuilding->id;
+
+            if ($taskSeed['parent_task_id'] > 0) {
+                $taskSeed['parent_task_id'] += $lastID;
+            }
+        }
+
+        return $taskSeeds;
     }
 }
