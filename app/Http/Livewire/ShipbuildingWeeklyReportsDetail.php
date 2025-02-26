709d0e92720b4388f62684b05827d2b759a66150
Fredy Nurman Saleh <email@fredyns.net>
2025-02-26 10:58:34 +0700

    livewire crud for shipbuilding.weekly-reports

---
+++ app/Http/Livewire/ShipbuildingWeeklyReportsDetail.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Shipbuilding;
+use App\Models\WeeklyReport;
+use Carbon\Carbon;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\View\View;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+use Livewire\WithPagination;
+
+class ShipbuildingWeeklyReportsDetail extends Component
+{
+    use WithPagination;
+    use WithFileUploads;
+    use AuthorizesRequests;
+
+    public Shipbuilding $shipbuilding;
+    public WeeklyReport $weeklyReport;
+    public $weeklyReportReportFile;
+    public $uploadIteration = 0;
+    public $weeklyReportDate;
+
+    public $selected = [];
+    public $editing = false;
+    public $allSelected = false;
+    public $showingModalView = false;
+    public $showingModalForm = false;
+
+    public $modalTitle = 'New WeeklyReport';
+
+    protected $rules = [
+        'weeklyReport.week' => ['required', 'numeric'],
+        'weeklyReportDate' => ['nullable', 'date'],
+        'weeklyReport.planned_progress' => ['nullable', 'numeric'],
+        'weeklyReport.actual_progress' => ['nullable', 'numeric'],
+        'weeklyReport.summary' => ['nullable', 'max:255', 'string'],
+        'weeklyReportReportFile' => ['file', 'max:1024', 'nullable'],
+    ];
+
+    public function mount(Shipbuilding $shipbuilding): void
+    {
+        $this->shipbuilding = $shipbuilding;
+        $this->resetWeeklyReportData();
+    }
+
+    public function resetWeeklyReportData(): void
+    {
+        $this->weeklyReport = new WeeklyReport();
+
+        $this->weeklyReportReportFile = null;
+        $this->weeklyReportDate = null;
+
+        $this->dispatchBrowserEvent('refresh');
+    }
+
+    public function newWeeklyReport(): void
+    {
+        $this->editing = false;
+        $this->modalTitle = trans('crud.shipbuilding_weekly_reports.new_title');
+        $this->resetWeeklyReportData();
+
+        $this->showModalForm();
+    }
+
+    public function viewWeeklyReport(WeeklyReport $weeklyReport): void
+    {
+        $this->editing = false;
+        $this->modalTitle = trans(
+            'crud.shipbuilding_weekly_reports.show_title'
+        );
+        $this->weeklyReport = $weeklyReport;
+
+        $this->weeklyReportDate = optional($this->weeklyReport->date)->format(
+            'Y-m-d'
+        );
+
+        $this->dispatchBrowserEvent('refresh');
+
+        $this->showModalView();
+    }
+
+    public function editWeeklyReport(WeeklyReport $weeklyReport): void
+    {
+        $this->editing = true;
+        $this->modalTitle = trans(
+            'crud.shipbuilding_weekly_reports.edit_title'
+        );
+        $this->weeklyReport = $weeklyReport;
+
+        $this->weeklyReportDate = optional($this->weeklyReport->date)->format(
+            'Y-m-d'
+        );
+
+        $this->dispatchBrowserEvent('refresh');
+
+        $this->showModalForm();
+    }
+
+    public function showModalView(): void
+    {
+        $this->resetErrorBag();
+        $this->showingModalView = true;
+        $this->showingModalForm = false;
+    }
+
+    public function showModalForm(): void
+    {
+        $this->resetErrorBag();
+        $this->showingModalView = false;
+        $this->showingModalForm = true;
+    }
+
+    public function hideModal(): void
+    {
+        $this->showingModalView = false;
+        $this->showingModalForm = false;
+    }
+
+    public function save(): void
+    {
+        $this->validate();
+
+        if (!$this->weeklyReport->shipbuilding_id) {
+            $this->authorize('create', WeeklyReport::class);
+
+            $this->weeklyReport->shipbuilding_id = $this->shipbuilding->id;
+        } else {
+            $this->authorize('update', $this->weeklyReport);
+        }
+
+        if ($this->weeklyReportReportFile) {
+            $this->weeklyReport->report_file = $this->weeklyReportReportFile->store(
+                'public'
+            );
+        }
+
+        $this->weeklyReport->date = Carbon::make(
+            $this->weeklyReportDate
+        );
+
+        $this->weeklyReport->save();
+
+        $this->uploadIteration++;
+
+        $this->hideModal();
+    }
+
+    public function destroySelected(): void
+    {
+        $this->authorize('delete-any', WeeklyReport::class);
+
+        collect($this->selected)->each(function (string $id) {
+            $weeklyReport = WeeklyReport::findOrFail($id);
+
+            if ($weeklyReport->report_file) {
+                Storage::delete($weeklyReport->report_file);
+            }
+
+            $weeklyReport->delete();
+        });
+
+        $this->selected = [];
+        $this->allSelected = false;
+
+        $this->resetWeeklyReportData();
+    }
+
+    public function toggleFullSelection(): void
+    {
+        if (!$this->allSelected) {
+            $this->selected = [];
+            return;
+        }
+
+        foreach ($this->shipbuilding->weeklyReports as $weeklyReport) {
+            array_push($this->selected, $weeklyReport->id);
+        }
+    }
+
+    public function render(): View
+    {
+        return view('livewire.shipbuilding-weekly-reports-detail', [
+            'weeklyReports' => $this->shipbuilding
+                ->weeklyReports()
+                ->paginate(100),
+        ]);
+    }
+}
---
+++ resources/views/livewire/shipbuilding-weekly-reports-detail.blade.php
@@ -0,0 +1,297 @@
+<div>
+    <div>
+        @can('create', App\Models\WeeklyReport::class)
+            <button class="button" wire:click="newWeeklyReport">
+                <i class="mr-1 icon ion-md-add text-primary"></i>
+                @lang('crud.common.new')
+            </button>
+        @endcan @can('delete-any', App\Models\WeeklyReport::class)
+            <button
+                class="button button-danger"
+                {{ empty($selected) ? 'disabled' : '' }}
+                onclick="confirm('{{ __('crud.common.are_you_sure') }}') || event.stopImmediatePropagation()"
+                wire:click="destroySelected"
+            >
+                <i class="mr-1 icon ion-md-trash text-primary"></i>
+                @lang('crud.common.delete_selected')
+            </button>
+        @endcan
+    </div>
+
+    <x-modal wire:model="showingModalView">
+        <div class="px-6 py-4">
+            <div class="text-lg font-bold">{{ $modalTitle }}</div>
+
+            <div class="mt-5">
+                <div class="flex flex-wrap">
+                    <div class="mb-4 w-full">
+                        <h5 class="font-medium text-gray-700">
+                            @lang('crud.shipbuilding_weekly_reports.inputs.week')
+                        </h5>
+                        <span> {{ $weeklyReport->week ?? '-' }} </span>
+                    </div>
+                    <div class="mb-4 w-full">
+                        <h5 class="font-medium text-gray-700">
+                            @lang('crud.shipbuilding_weekly_reports.inputs.date')
+                        </h5>
+                        <span>
+                            {{ optional($weeklyReport->date)->format('l, d F Y')
+                            }}
+                        </span>
+                    </div>
+                    <div class="mb-4 w-full">
+                        <h5 class="font-medium text-gray-700">
+                            @lang('crud.shipbuilding_weekly_reports.inputs.planned_progress')
+                        </h5>
+                        <span>
+                            {{ $weeklyReport->planned_progress ?? '-' }}
+                        </span>
+                    </div>
+                    <div class="mb-4 w-full">
+                        <h5 class="font-medium text-gray-700">
+                            @lang('crud.shipbuilding_weekly_reports.inputs.actual_progress')
+                        </h5>
+                        <span>
+                            {{ $weeklyReport->actual_progress ?? '-' }}
+                        </span>
+                    </div>
+                    <div class="mb-4 w-full">
+                        <h5 class="font-medium text-gray-700">
+                            @lang('crud.shipbuilding_weekly_reports.inputs.summary')
+                        </h5>
+                        <span> {{ $weeklyReport->summary ?? '-' }} </span>
+                    </div>
+                    <div class="mb-4 w-full">
+                        <h5 class="font-medium text-gray-700">
+                            @lang('crud.shipbuilding_weekly_reports.inputs.report_file')
+                        </h5>
+                        @if($weeklyReport->report_file)
+                            <a
+                                href="{{ \Storage::url($weeklyReport->report_file) }}"
+                                target="blank"
+                            >
+                                <i class="mr-1 icon ion-md-download"></i>
+                                Download
+                            </a>
+                        @else
+                            -
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="px-6 py-4 bg-gray-50 flex justify-between">
+            <button
+                type="button"
+                class="button"
+                wire:click="$toggle('showingModalView')"
+            >
+                <i class="mr-1 icon ion-md-close"></i>
+                @lang('text.close')
+            </button>
+            @can('update', $weeklyReport)
+                <button
+                    type="button"
+                    class="button mr-1"
+                    wire:click="editWeeklyReport('{{ $weeklyReport->id }}')"
+                >
+                    <i class="mr-1 icon ion-md-create"></i>
+                    @lang('crud.common.edit')
+                </button>
+            @endcan
+        </div>
+    </x-modal>
+
+    <x-modal wire:model="showingModalForm">
+        <div class="px-6 py-4">
+            <div class="text-lg font-bold">{{ $modalTitle }}</div>
+
+            <div class="mt-5">
+                <div class="flex flex-wrap">
+                    <x-inputs.group class="w-full">
+                        <x-inputs.slider
+                            name="weeklyReport.week"
+                            wire:model="weeklyReport.week"
+                            label="{{ __('crud.weekly_reports.inputs.week') }}"
+                            placeholder="{{ __('crud.weekly_reports.inputs.week') }}"
+                        ></x-inputs.slider>
+                    </x-inputs.group>
+                    <x-inputs.group class="w-full">
+                        <x-inputs.date
+                            name="weeklyReportDate"
+                            wire:model="weeklyReportDate"
+                            label="{{ __('crud.weekly_reports.inputs.date') }}"
+                            placeholder="{{ __('crud.weekly_reports.inputs.date') }}"
+                        ></x-inputs.date>
+                    </x-inputs.group>
+                    <x-inputs.group class="w-full">
+                        <x-inputs.slider
+                            name="weeklyReport.planned_progress"
+                            wire:model="weeklyReport.planned_progress"
+                            label="{{ __('crud.weekly_reports.inputs.planned_progress') }}"
+                            placeholder="{{ __('crud.weekly_reports.inputs.planned_progress') }}"
+                            step="0.01"
+                        ></x-inputs.slider>
+                    </x-inputs.group>
+                    <x-inputs.group class="w-full">
+                        <x-inputs.slider
+                            name="weeklyReport.actual_progress"
+                            wire:model="weeklyReport.actual_progress"
+                            label="{{ __('crud.weekly_reports.inputs.actual_progress') }}"
+                            placeholder="{{ __('crud.weekly_reports.inputs.actual_progress') }}"
+                            step="0.01"
+                        ></x-inputs.slider>
+                    </x-inputs.group>
+                    <x-inputs.group class="w-full">
+                        <x-inputs.textarea
+                            name="weeklyReport.summary"
+                            wire:model="weeklyReport.summary"
+                            label="{{ __('crud.weekly_reports.inputs.summary') }}"
+                            placeholder="{{ __('crud.weekly_reports.inputs.summary') }}"
+                        >
+                            {{ old('summary', ($editing ? $weeklyReport->summary
+                            : '')) }}
+                        </x-inputs.textarea>
+                    </x-inputs.group>
+                    <x-inputs.group class="w-full">
+                        <x-inputs.partials.label
+                            name="weeklyReportReportFile"
+                            label="{{ __('crud.weekly_reports.inputs.report_file') }}"
+                        ></x-inputs.partials.label>
+                        <br/>
+
+                        <input
+                            type="file"
+                            name="weeklyReportReportFile"
+                            id="weeklyReportReportFile{{ $uploadIteration }}"
+                            wire:model="weeklyReportReportFile"
+                            class="form-control-file"
+                        />
+
+                        @if($editing && $weeklyReport->report_file)
+                            <div class="mt-2">
+                                <a
+                                    href="{{ \Storage::url($weeklyReport->report_file) }}"
+                                    target="_blank"
+                                >
+                                    <i class="icon ion-md-download"></i>
+                                    Download
+                                </a>
+                            </div>
+                        @endif @error('weeklyReportReportFile')
+                        @include('components.inputs.partials.error') @enderror
+                    </x-inputs.group>
+                </div>
+            </div>
+        </div>
+
+        <div class="px-6 py-4 bg-gray-50 flex justify-between">
+            <button
+                type="button"
+                class="button"
+                wire:click="$toggle('showingModalForm')"
+            >
+                <i class="mr-1 icon ion-md-close"></i>
+                @lang('crud.common.cancel')
+            </button>
+
+            <button
+                type="button"
+                class="button button-primary"
+                wire:click="save"
+            >
+                <i class="mr-1 icon ion-md-save"></i>
+                @lang('crud.common.save')
+            </button>
+        </div>
+    </x-modal>
+
+    <div class="block w-full overflow-auto scrolling-touch mt-4">
+        <table class="w-full max-w-full mb-4 bg-transparent">
+            <thead class="text-gray-700">
+            <tr>
+                <th class="px-4 py-3 text-left w-1">
+                    <input
+                        type="checkbox"
+                        wire:model="allSelected"
+                        wire:click="toggleFullSelection"
+                        title="{{ trans('crud.common.select_all') }}"
+                    />
+                </th>
+                <th class="px-4 py-3 text-right">
+                    @lang('crud.shipbuilding_weekly_reports.inputs.week')
+                </th>
+                <th class="px-4 py-3 text-left">
+                    @lang('crud.shipbuilding_weekly_reports.inputs.date')
+                </th>
+                <th class="px-4 py-3 text-right">
+                    @lang('crud.shipbuilding_weekly_reports.inputs.planned_progress')
+                </th>
+                <th class="px-4 py-3 text-right">
+                    @lang('crud.shipbuilding_weekly_reports.inputs.actual_progress')
+                </th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody class="text-gray-600">
+            @foreach ($weeklyReports as $weeklyReport)
+                <tr class="hover:bg-gray-100">
+                    <td class="px-4 py-3 text-left">
+                        <input
+                            type="checkbox"
+                            value="{{ $weeklyReport->id }}"
+                            wire:model="selected"
+                        />
+                    </td>
+                    <td class="px-4 py-3 text-right">
+                        {{ $weeklyReport->week ?? '-' }}
+                    </td>
+                    <td class="px-4 py-3 text-left">
+                        {{ optional($weeklyReport->date)->format('D, d M Y') }}
+                    </td>
+                    <td class="px-4 py-3 text-right">
+                        {{ $weeklyReport->planned_progress ?? '-' }}
+                    </td>
+                    <td class="px-4 py-3 text-right">
+                        {{ $weeklyReport->actual_progress ?? '-' }}
+                    </td>
+                    <td class="px-4 py-3 text-right" style="width: 134px;">
+                        <div
+                            role="group"
+                            aria-label="Row Actions"
+                            class="relative inline-flex align-middle"
+                        >
+                            <button
+                                type="button"
+                                class="button mr-1"
+                                wire:click="viewWeeklyReport('{{ $weeklyReport->id }}')"
+                            >
+                                <i class="icon ion-md-eye"></i>
+                            </button>
+                            @can('update', $weeklyReport)
+                                <button
+                                    type="button"
+                                    class="button mr-1"
+                                    wire:click="editWeeklyReport('{{ $weeklyReport->id }}')"
+                                >
+                                    <i class="icon ion-md-create"></i>
+                                </button>
+                            @endcan
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+            <tfoot>
+            <tr>
+                <td colspan="5">
+                    <div class="mt-10 px-4">
+                        {{ $weeklyReports->render() }}
+                    </div>
+                </td>
+            </tr>
+            </tfoot>
+        </table>
+    </div>
+</div>
