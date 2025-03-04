58b5ca4cd8482b0c17f23116bf19c0386bd9e560
Fredy Nurman Saleh <email@fredyns.net>
2025-03-04 15:46:28 +0700

    default to dashboard

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/dashboard');
 });
 
 Route::get('/email/verify', function () {
